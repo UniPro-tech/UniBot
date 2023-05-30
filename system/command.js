97f0ff2bd84e3c4b432b6ed36373c669601966fe
唯人 <yuito_it@outlook.jp>
2023-05-30 19:16:00 +0900

    test

--- system/command.js
+++ system/command.js
@@ -4,7 +4,7 @@ module.exports = {
         client.commands = new Collection();
         const commandFolders = fs.readdirSync(`./commands`);
         for (const folder of commandFolders) {
-            console.log(`===${folder} commands===`);
+            console.log(`\u001b[32m===${folder} commands===\u001b[0m`);
             const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith(".js"));
             for (const file of commandFiles) {
                 console.log(`dir:${folder},file:${file}`);
@@ -13,13 +13,13 @@ module.exports = {
                 }
                 catch{
                     console.log("error!!");
-                }/*
+                }
                 try {
                     client.commands.set(command.data.name, command);
                     console.log(`${command.data.name} がロードされました。`);
                 } catch (error) {
                     console.log(`\u001b[31m${command.data.name} はエラーによりロードされませんでした。\nエラー内容\n ${error}\u001b[0m`);
-                }*/
+                }
             }
             console.log(`\u001b[32m===${folder} loaded===\u001b[0m`);
         }
