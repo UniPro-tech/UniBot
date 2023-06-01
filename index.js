bc4fd29f1cc86cf5281b3d96ab345392725cf870
唯人 <yuito_it@outlook.jp>
2023-06-01 20:22:25 +0900

    bug test

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ client.on("interactionCreate", async i => {
       .setDescription("```\n" + error + "\n```")
       .setColor(config.color.e)
       .setTimestamp();
-    i.reply(config.logch.error).then(c => c.send({ embeds: [iEmbed] }));
+    i.reply(config.logch.error).then(c => config.logch.command.send({ embeds: [iEmbed] }));
   }
 });
 client.login(config.token);
