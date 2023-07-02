552593023f7e2113f3f20202fa30560c5455d4c6
唯人 <yuito_it@outlook.jp>
2023-07-02 16:16:41 +0900

    bug-fix

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ client.on("interactionCreate", async i => {
 
   // 実行
   try {
+    command.execute(i,client);
     const log = new EmbedBuilder()
       .setTitle("コマンド実行ログ")
       .setDescription(`${i.user.tag}(${i.user.id}) がコマンドを実行しました。`)
