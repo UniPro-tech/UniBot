9df34c01154ecc777ee63c69995cbe99d106e7c7
唯人 <yuito_it@outlook.jp>
2023-07-27 14:38:51 +0900

    test

--- commands/general/avatar.js
+++ commands/general/avatar.js
@@ -1,23 +1,35 @@
 const { SlashCommandBuilder } = require("discord.js");
+const Discord = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("avatar")
     .setDescription(
-      "Get the avatar URL of the selected user, or your own avatar."
+      "アイコンのURLを取得します。"
     )
     .addUserOption((option) =>
-      option.setName("target").setDescription("The user's avatar to show")
+      option.setName("target").setDescription("ここにユーザーを指定してそのユーザーのアイコンのURLを取得します。")
     ),
-  async execute(interaction) {
-    const user = interaction.options.getUser("target");
-    if (user)
-      return interaction.reply(
-        `${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`
-      );
-    interaction.reply(
-      `Your avatar: ${interaction.user.displayAvatarURL()}`
-    );
-    return `No data`;
+  async execute(i) {
+    const user = i.options.getUser("target");
+    if (user){
+      const embed = new Discord.EmbedBuilder()
+      .setTitle(`${user.username}'s Avatar`)
+      .setDescription(`URL:${user.displayAvatarURL({ dynamic: true })}`)
+      .setImage('https://i.imgur.com/AfFp7pu.png')
+      .setColor(client.conf.color.s)
+      .setTimestamp();
+      i.reply({ embeds: [embed] });
+    }
+    else{
+      const embed = new Discord.EmbedBuilder()
+      .setTitle(`${user.username}'s Avatar`)
+      .setDescription(`URL:${user.displayAvatarURL({ dynamic: true })}`)
+      .setImage(`${user.displayAvatarURL({ dynamic: true })}`)
+      .setColor(client.conf.color.s)
+      .setTimestamp();
+      i.reply({ embeds: [embed] });
+    }
+    return 'No data';
   },
 };
