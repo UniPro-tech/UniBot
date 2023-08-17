b588e38e4dbd0586fb4e479a1706895f3666227c
唯人 <yuito_it@outlook.jp>
2023-08-17 11:56:51 +0900

    test

--- events/ready.js
+++ events/ready.js
@@ -3,10 +3,36 @@ module.exports = {
   async execute(client) {
     const log = await client.func.readLog("v1/conf/status");
     const add = require(`../system/add.js`);
+
     add.addCmd(client.conf);
     console.log(`on:${log.onoff},play:${log.playing},status:${log.status}`);
+
     if (log.onoff == 'on') {
-      client.user.setActivity(log.playing, { type: log.activity });
+
+      let activityOpt;
+      switch (log.type) {
+        case "WATCHING":
+          activityOpt.type = Discord.ActivityType.Watching;
+          break;
+
+        case "COMPETING":
+          activityOpt.type = Discord.ActivityType.Competing;
+          break
+
+        case "LISTENING":
+          activityOpt.type = Discord.ActivityType.Listening;
+          break;
+
+        case "STREAMING":
+          activityOpt.type = Discord.ActivityType.Streaming;
+          break;
+
+        default:
+          activityOpt.type = Discord.ActivityType.Playing;
+          break;
+      }
+
+      client.user.setActivity(log.playing, activityOpt);
       if (log.status == 'Discord Android') {
         client.ws = { properties: { $browser: 'Discord Android' } };
       } else {
@@ -21,13 +47,6 @@ module.exports = {
       //client.ws = { properties: { "$os": "Untitled OS", "$browser": "Untitled Browser", "$device": "Replit Container" } };
       client.user.setStatus('online');
     }
-    /*client.user.setActivity('activity', { type: 'WATCHING' });
-client.user.setActivity('activity', { type: 'LISTENING' });
-client.user.setActivity('activity', { type: 'COMPETING' });*/
-    /*client.user.setStatus('online');
-client.user.setStatus('idle');
-client.user.setStatus('dnd');
-client.user.setStatus('invisible');*/
     client.channels.cache.get(client.conf.logch.ready).send("Discordログインしました!");
     console.log(`[${client.func.timeToJST(Date.now(), true)}] Logged in as ${client.user.tag}!`);
   }
