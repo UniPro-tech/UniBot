bd59fc61333a9e421532ed854b990a650543273a
唯人(Yuito Akatsuki - JP) <yuito_it@outlook.jp>
2023-10-10 20:45:08 +0900

    ✨ New Command:About - /aboutでこのBotに対する情報を提供します。

---
+++ commands/general/about.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require("discord.js");
+const Discord = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("about")
+        .setDescription(
+            "アイコンのURLを取得します。"
+        ),
+    async execute(i, client) {
+        let size;
+        client.shard.fetchClientValues('guilds.cache.size')
+            .then(result => {
+                size = result;
+            });
+        const embed = new Discord.EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle(`About ${client.conf.productname}`)
+            //.setURL('https://discord.js.org/')
+            .setAuthor(client.conf.author)
+            .setDescription(client.conf.description)
+            .setThumbnail(client.user.displayAvatarURL)
+            .addFields(
+                { name: 'Version', value: client.conf.version },
+                { name: 'Author', value: client.conf.author.name },
+                { name: 'Guild Count', value: result }
+            )
+            .setTimestamp();
+        i.reply({ embeds: [embed] });
+    }
+}
