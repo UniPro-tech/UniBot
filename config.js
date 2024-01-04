376acdb5b2aa472ddac2fd098cac88a7252ef6f3
yuito-it <yuito_it@outlook.jp>
2024-01-04 23:44:15 +0000

    gitignore changed

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  author: {
+    name: "YuitoAkatsukiJP",
+    url: "https://x.com/yuito_it_",
+    iconURL: "https://cdn.discordapp.com/avatars/95711041089.webp"
+  },
+  description: "UntitledBotは曙唯人が作った多機能Botです。現時点ではまだ機能は全然ありませんが、温かい目で見ていただければ幸いです。",
+  version: "v1.0.0",
+  productname: "UntitledBot",
+  clientId: "1110163628222591107",
+  color: {
+    s: 0x1bff49,
+    e: 0xff0000,
+  },
+  token: process.env.DISCORD_TOKEN,
+  dev: {
+    testGuild: "957110890828820500"
+  },
+  logch: {
+    ready: "1111590903015030785",
+    command: "1111590903015030785",
+    error: "1111590903015030785",
+    guildCreate: "1111590903015030785",
+    guildDelete: "1111590903015030785"
+  }
+}
