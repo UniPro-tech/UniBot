fd6715adef6f79d5798fa346db1b2593bd1ce5e7
YuitoAkatsuki <yuito_it@outlook.jp>
2024-04-27 09:16:47 +0900

    refactor: README編集とコード整形

--- config.js
+++ config.js
@@ -2,9 +2,10 @@ module.exports = {
   author: {
     name: "YuitoAkatsukiJP",
     url: "https://x.com/UniProject_digital",
-    iconURL: "https://cdn.discordapp.com/avatars/95711041089.webp"
+    iconURL: "https://cdn.discordapp.com/avatars/95711041089.webp",
   },
-  description: "UntitledBotはデジタルサークルUniProjectが作った多機能Botです。現時点ではまだ機能は全然ありませんが、温かい目で見ていただければ幸いです。",
+  description:
+    "UntitledBotはデジタルサークルUniProjectが作った多機能Botです。現時点ではまだ機能は全然ありませんが、温かい目で見ていただければ幸いです。",
   version: "v1.1.0",
   productname: "UntitledBot",
   clientId: "1110163628222591107",
@@ -14,13 +15,13 @@ module.exports = {
   },
   token: process.env.DISCORD_TOKEN,
   dev: {
-    testGuild: "1191346186880286770"
+    testGuild: "1191346186880286770",
   },
   logch: {
     ready: "1228574771856543835",
     command: "1228574771856543835",
     error: "1228574771856543835",
     guildCreate: "1228574771856543835",
-    guildDelete: "1228574771856543835"
-  }
-}
+    guildDelete: "1228574771856543835",
+  },
+};
--- function.js
+++ function.js
@@ -2,62 +2,68 @@ const URI_base = process.env.LOGING_URI_BASE;
 
 //タイムスタンプをJSTタイムスタンプに変換
 function timeToJSTTimestamp(timestamp) {
-    var dt = new Date(); //Date オブジェクトを作成
-    var tz = dt.getTimezoneOffset(); //サーバーで設定されているタイムゾーンの世界標準時からの時差（分）
-    tz = (tz + 540) * 60 * 1000; //日本時間との時差(9時間=540分)を計算し、ミリ秒単位に変換
+  var dt = new Date(); //Date オブジェクトを作成
+  var tz = dt.getTimezoneOffset(); //サーバーで設定されているタイムゾーンの世界標準時からの時差（分）
+  tz = (tz + 540) * 60 * 1000; //日本時間との時差(9時間=540分)を計算し、ミリ秒単位に変換
 
-    dt = new Date(timestamp + tz); //時差を調整した上でタイムスタンプ値を Date オブジェクトに変換
-    return dt;
+  dt = new Date(timestamp + tz); //時差を調整した上でタイムスタンプ値を Date オブジェクトに変換
+  return dt;
 }
 exports.timeToJSTTimestamp = timeToJSTTimestamp;
 
 //JSTタイムスタンプから日付
 exports.timeToJST = function (timestamp, format = false) {
-    const dt = timeToJSTTimestamp(timestamp);
-    const year = dt.getFullYear();
-    const month = dt.getMonth() + 1
-    const date = dt.getDate();
-    const hour = dt.getHours();
-    const minute = dt.getMinutes();
-    const second = dt.getSeconds();
+  const dt = timeToJSTTimestamp(timestamp);
+  const year = dt.getFullYear();
+  const month = dt.getMonth() + 1;
+  const date = dt.getDate();
+  const hour = dt.getHours();
+  const minute = dt.getMinutes();
+  const second = dt.getSeconds();
 
-    let return_str;
-    if (format == true) {
-        return_str = `${year}/${month}/${date} ${hour}:${minute}:${second}`;
-    } else {
-        return_str = { "year": year, "month": month, "date": date, "hour": hour, "minute": minute, "second": second };
-    }
-    return return_str;
-}
+  let return_str;
+  if (format == true) {
+    return_str = `${year}/${month}/${date} ${hour}:${minute}:${second}`;
+  } else {
+    return_str = {
+      year: year,
+      month: month,
+      date: date,
+      hour: hour,
+      minute: minute,
+      second: second,
+    };
+  }
+  return return_str;
+};
 
 /*LogingAPIにポストして、DBにプッシュする。
-*post_data = ポストするためのjson(object)
-*api_name = URI
-*/
-const axios = require('axios');
-const https = require('https');
+ *post_data = ポストするためのjson(object)
+ *api_name = URI
+ */
+const axios = require("axios");
+const https = require("https");
 exports.readLog = async (api_name) => {
-    const URI = `https://${URI_base}/${api_name}`;
-    try {
-        const response = await axios.get(URI);
-        const ret = JSON.stringify(response.data);
-        console.log('レスポンス:', response.data);
-        return JSON.parse(ret);
-    } catch (error) {
-        console.error('エラー:', error.message);
-    }
-
-}
-const http = require('http')
+  const URI = `https://${URI_base}/${api_name}`;
+  try {
+    const response = await axios.get(URI);
+    const ret = JSON.stringify(response.data);
+    console.log("レスポンス:", response.data);
+    return JSON.parse(ret);
+  } catch (error) {
+    console.error("エラー:", error.message);
+  }
+};
+const http = require("http");
 
 exports.loging = async (post_data, api_name) => {
-    try {
-        const url = `https://${URI_base}/${api_name}`;
+  try {
+    const url = `https://${URI_base}/${api_name}`;
 
-        const response = await axios.post(url, post_data);
+    const response = await axios.post(url, post_data);
 
-        console.log('Response:', response.data);
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
-}
+    console.log("Response:", response.data);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+};
