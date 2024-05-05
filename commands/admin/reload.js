af2476247089269dbc093d33fc48b027be8705d9
YuitoAkatsuki <yuito_it@outlook.jp>
2024-05-05 18:59:11 +0900

    :recycle: 管理コマンドの場合、権限の有無を確認するようにする

--- commands/admin/reload.js
+++ commands/admin/reload.js
@@ -4,13 +4,11 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("reload")
     .setDescription("Reloads a command."),
-  /*.addStringOption((option) =>
-      option
-        .setName("command")
-        .setDescription("The command to reload.")
-        .setRequired(true)
-  */
   async execute(i, client) {
+    if (!i.member.roles.cache.has(client.conf.adminRoleId)) {
+      i.reply("権限がありません");
+      return;
+    }
     i.reply("Now rebooting...");
     process.exit(1);
   },
