d496987fd1f122a2c83a8b932364f1e60acd0856
YuitoAkatsuki <yuito_it@outlook.jp>
2024-05-05 21:15:05 +0900

    :bug: envの整数化

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ module.exports = {
     testGuild: process.env.ADMIN_GUILD,
   },
   logch: {
-    ready: process.env.LOG_READY_ID,
-    command: process.env.LOG_COMMAND_ID,
-    error: process.env.LOG_ERROR_ID,
-    guildCreate: process.env.LOG_GUILD_JOIN_ID,
-    guildDelete: process.env.LOG_GUID_LEAVE_ID,
+    ready: Number(process.env.LOG_READY_ID),
+    command: Number(process.env.LOG_COMMAND_ID),
+    error: Number(process.env.LOG_ERROR_ID),
+    guildCreate: Number(process.env.LOG_GUILD_JOIN_ID),
+    guildDelete: Number(process.env.LOG_GUID_LEAVE_ID),
   },
   URI_base: process.env.URI_BASE,
 };
