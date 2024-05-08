f30a39e9cd22a8eaf3d4c54b8d819281a70c2032
reno <reno@usernoMacBook-Pro.local>
2024-05-09 04:08:48 +0900

    インタラクションイベントをファイル分け

--- commands/general/about.js
+++ commands/general/about.js
@@ -1,28 +1,24 @@
-const { SlashCommandBuilder } = require("discord.js");
-const Discord = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("about")
     .setDescription("このBotについての情報を表示します。"),
-  async execute(i, client) {
-    let size;
-    client.shard.fetchClientValues("guilds.cache.size").then((result) => {
-      size = result;
-    });
-    const embed = new Discord.EmbedBuilder()
+  async execute(interaction) {
+    const size = await interaction.client.shard.fetchClientValues("guilds.cache.size");
+    const embed = new EmbedBuilder()
       .setColor(0x0099ff)
-      .setTitle(`About ${client.conf.productname}`)
+      .setTitle(`About ${interaction.client.conf.productname}`)
       .setURL("https://uniproject-tech.net/UniBot/")
-      .setAuthor(client.conf.author)
-      .setDescription(client.conf.description)
-      .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+      .setAuthor(interaction.client.conf.author)
+      .setDescription(interaction.client.conf.description)
+      .setThumbnail(interaction.client.user.displayAvatarURL({ dynamic: true }))
       .addFields(
-        { name: "Version", value: client.conf.version },
-        { name: "Author", value: client.conf.author.name },
+        { name: "Version", value: interaction.client.conf.version },
+        { name: "Author", value: interaction.client.conf.author.name },
         { name: "Guild Count", value: `${size}` }
       )
       .setTimestamp();
-    i.reply({ embeds: [embed] });
+    interaction.reply({ embeds: [embed] });
   },
 };
---
+++ events/interactionCreate.js
@@ -0,0 +1,89 @@
+const { Events, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name: Events.InteractionCreate,
+    async execute(interaction) {
+        if (interaction.isChatInputCommand()) {
+            console.log(`[!] ${interaction.commandName}`);
+            const command = interaction.client.commands.get(interaction.commandName);
+            if (!command) {
+                console.log(`[-] Not Found: ${interaction.commandName}`);
+                return;
+            }
+            if (!interaction.inGuild() && command.guildOnly) {
+                const embed = new EmbedBuilder()
+                    .setTitle("エラー")
+                    .setDescription("このコマンドはDMでは実行できません。")
+                    .setColor(interaction.client.config.color.e);
+                interaction.reply({ embeds: [embed] });
+                return;
+            }
+
+            try {
+                await command.execute(interaction);
+                console.log(`[Run] ${interaction.commandName}`);
+
+                const logEmbed = new EmbedBuilder()
+                    .setTitle("コマンド実行ログ")
+                    .setDescription(`${interaction.user} がコマンドを実行しました。`)
+                    .setColor(interaction.client.conf.color.s)
+                    .setTimestamp()
+                    .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
+                    .addFields([
+                        {
+                            name: "コマンド",
+                            value: `\`\`\`\n/${interaction.commandName}\n\`\`\``,
+                        },
+                        {
+                            name: "実行サーバー",
+                            value: "```\n"
+                                + interaction.inGuild() ? `${interaction.guild.name} (${interaction.guild.id})` : "DM"
+                            + "\n```",
+                        },
+                        {
+                            name: "実行ユーザー",
+                            value: "```\n" + `${interaction.user.tag}(${interaction.user.id})` + "\n```",
+                        },
+                    ])
+                    .setFooter({ text: `${interaction.id}` });
+                const channel = await GetLogChannel(interaction);
+                if (channel) {
+                    channel.send({ embeds: [logEmbed] });
+                }
+            } catch (error) {
+                console.error(error);
+                const logEmbed = new EmbedBuilder()
+                    .setTitle("ERROR - cmd")
+                    .setDescription("```\n" + error.toString() + "\n```")
+                    .setColor(interaction.conf.color.e)
+                    .setTimestamp();
+
+                const channel = await GetErrorChannel(interaction);
+                if (channel) {
+                    channel.send({ embeds: [logEmbed] });
+                }
+                const messageEmbed = new EmbedBuilder()
+                    .setTitle("すみません、エラーが発生しました...")
+                    .setDescription("```\n" + error + "\n```")
+                    .setColor(interaction.conf.color.e)
+                    .setTimestamp();
+
+                await interaction.reply(messageEmbed);
+                const logChannel = await GetLogChannel(interaction);
+                if (logChannel) {
+                    logChannel.send({ embeds: [messageEmbed] });
+                }
+            }
+        }
+    }
+};
+
+const GetLogChannel = async (interaction) => {
+    const channel = await interaction.client.channels.fetch(interaction.client.conf.logch.command).catch((error) => null);
+    return channel;
+};
+
+const GetErrorChannel = async (interaction) => {
+    const channel = await interaction.client.channels.fetch(interaction.client.conf.logch.error).catch((error) => null);
+    return channel;
+};
