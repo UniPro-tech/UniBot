7453af0798f2d37626f4588ce5970dfa0fc631c6
唯人(Yuito Akatsuki) <yuito_it@outlook.jp>
2024-08-27 23:29:34 +0900

    Refactor log format (#85)
    :recycle: log format

--- commands/general/about.js
+++ commands/general/about.js
@@ -12,33 +12,59 @@ module.exports = {
       .setTitle(`About ${package_data.name}`)
       .setTimestamp();
     if (interaction.client.shard) {
-      embed.addFields({name: "Shard ID", value: interaction.client.shard.ids, inline: true});
-      embed.addFields({name: "Shard Count", value: interaction.client.shard.count, inline: true});
+      embed.addFields({
+        name: "Shard ID",
+        value: interaction.client.shard.ids,
+        inline: true,
+      });
+      embed.addFields({
+        name: "Shard Count",
+        value: interaction.client.shard.count,
+        inline: true,
+      });
     }
     if (package_data.description) {
       embed.setDescription(package_data.description);
     }
     if (package_data.version) {
-      embed.addFields({ name: "Version", value: package_data.version, inline: true});
+      embed.addFields({
+        name: "Version",
+        value: package_data.version,
+        inline: true,
+      });
     }
     if (package_data.author) {
-      embed.addFields({name: "Author", value: `[${package_data.author + package_data.email}](${package_data.author.url})`});
+      embed.addFields({
+        name: "Author",
+        value: `[${package_data.author + package_data.email}](${
+          package_data.author.url
+        })`,
+      });
     }
     if (package_data.license) {
-      embed.addFields({name: "License", value: package_data.license, inline: true});
+      embed.addFields({
+        name: "License",
+        value: package_data.license,
+        inline: true,
+      });
     }
     if (package_data.repository) {
-      embed.addFields({name: "Repository", value: package_data.repository.url});
+      embed.addFields({
+        name: "Repository",
+        value: package_data.repository.url,
+      });
     }
     if (package_data.homepage) {
       embed.setURL(package_data.homepage);
     }
-    if (package_data.contributors) { 
+    if (package_data.contributors) {
       let temp = new Array();
-      for(let i = 0; i < package_data.contributors.length; i++) {
-        temp[i] = `[${ package_data.contributors[i].name} <${package_data.contributors[i].email }>](${ package_data.contributors[i].url })`;
+      for (let i = 0; i < package_data.contributors.length; i++) {
+        temp[
+          i
+        ] = `[${package_data.contributors[i].name} <${package_data.contributors[i].email}>](${package_data.contributors[i].url})`;
       }
-      embed.addFields({name: "Contributors", value: temp.join("\n")});
+      embed.addFields({ name: "Contributors", value: temp.join("\n") });
     }
     interaction.reply({ embeds: [embed] });
   },
