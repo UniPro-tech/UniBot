00ca35206da911f165207017ddfc681d0bd44511
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-04-02 23:32:42 +0900

    rebootコマンドの実装を修正し、confをconfigに変更。エクスポート形式を統一。

--- src/commands/admin/maintenance/reboot.ts
+++ src/commands/admin/maintenance/reboot.ts
@@ -3,23 +3,27 @@ import {
   GuildMemberRoleManager,
   SlashCommandSubcommandBuilder,
 } from "discord.js";
-import conf from "@/config";
+import config from "@/config";
 
-module.exports = {
-  data: new SlashCommandSubcommandBuilder()
-    .setName("reboot")
-    .setDescription("Reboot."),
-  adminGuildOnly: true,
-  async execute(interaction: CommandInteraction) {
-    if (
-      !(interaction.member?.roles as GuildMemberRoleManager).cache.has(
-        conf.adminRoleId
-      )
-    ) {
-      interaction.reply("権限がありません");
-      return;
-    }
-    await interaction.reply("Now rebooting...");
-    process.exit(1);
-  },
+export const data = new SlashCommandSubcommandBuilder()
+  .setName("reboot")
+  .setDescription("Reboot.");
+export const adminGuildOnly = true;
+export const execute = async (interaction: CommandInteraction) => {
+  if (
+    !(interaction.member?.roles as GuildMemberRoleManager).cache.has(
+      config.adminRoleId
+    )
+  ) {
+    interaction.reply("権限がありません");
+    return;
+  }
+  await interaction.reply("Now rebooting...");
+  process.exit(1);
+};
+
+export default {
+  data,
+  adminGuildOnly,
+  execute,
 };
