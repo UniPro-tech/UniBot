2712128e2edd7615f73575325eb501f02c4cc7c0
Shinahin <tailangrongyi@gmail.com>
2025-04-21 00:58:51 +0900

    add reactionpanel

---
+++ src/commands/admin/rp/remove.ts
@@ -0,0 +1,25 @@
+import {
+  CommandInteraction,
+  GuildMemberRoleManager,
+  SlashCommandSubcommandBuilder,
+} from "discord.js";
+
+import config from "@/config";
+
+export const data = new SlashCommandSubcommandBuilder()
+  .setName("remove")
+  .setDescription("ReactionPanelを削除します");
+export const adminGuildOnly = true;
+export const execute = async (interaction: CommandInteraction) => {
+  if (!(interaction.member?.roles as GuildMemberRoleManager).cache.has(config.adminRoleId)) {
+    interaction.reply("権限がありません");
+    return;
+  }
+  await interaction.reply("作成中");
+};
+
+export default {
+  data,
+  adminGuildOnly,
+  execute,
+};
