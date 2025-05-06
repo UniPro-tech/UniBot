f15d9de7a22f415145c94559b4035bc00c20e5ff
yuito-it <yuito@uniproject.jp>
2025-05-06 10:12:16 +0000

    :sparkles: 役職の追加/削除処理を改善し、エラーハンドリングを強化

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -101,62 +101,143 @@ export const execute = async (interaction: Interaction) => {
       }
     }
   } else if (interaction.isStringSelectMenu()) {
-    if (interaction.customId.startsWith("rp_")) {
-      const selected = interaction.values;
-      console.log(
-        `[${interaction.client.function.timeUtils.timeToJSTstamp(
-          Date.now(),
-          true
-        )} info] -> Menu Selected: ${selected}`
-      );
-      await interaction.deferUpdate();
-      const member = interaction.guild?.members.cache.get(interaction.user.id);
-      if (!member) {
-        await interaction.followUp({
-          content: "メンバー情報を取得できませんでした。",
-          flags: MessageFlags.Ephemeral,
-        });
-        return;
-      }
-      interaction.editReply({
-        components: interaction.message.components,
-      });
-      selected.forEach(async (value) => {
-        const hasRole = member.roles.cache.has(value);
-
-        if (hasRole) {
-          await member.roles.remove(value);
+    try {
+      if (interaction.customId.startsWith("rp_")) {
+        const selected = interaction.values;
+        console.log(
+          `[${interaction.client.function.timeUtils.timeToJSTstamp(
+            Date.now(),
+            true
+          )} info] -> Menu Selected: ${selected}`
+        );
+        await interaction.deferUpdate();
+        const member = interaction.guild?.members.cache.get(interaction.user.id);
+        if (!member) {
           await interaction.followUp({
-            content: `${member.displayName} から役職 <@&${value}> を削除しました。`,
+            content: "メンバー情報を取得できませんでした。",
             flags: MessageFlags.Ephemeral,
           });
-          console.log(
-            `[${interaction.client.function.timeUtils.timeToJSTstamp(
-              Date.now(),
-              true
-            )} info] -> Role Removed: for ${member.displayName}`
-          );
-        } else {
-          await member.roles.add(value);
-          await interaction.followUp({
-            content: `${member.displayName} に役職 <@&${value}> を付与しました。`,
-            flags: MessageFlags.Ephemeral,
-          });
-          console.log(
-            `[${interaction.client.function.timeUtils.timeToJSTstamp(
-              Date.now(),
-              true
-            )} info] -> Role Added: for ${member.displayName}`
-          );
+          return;
         }
-      });
-    } else {
-      console.log(
+        interaction.editReply({
+          components: interaction.message.components,
+        });
+
+        let completedRoles = Array<{ roleId: string; action: string }>();
+        let completed = false;
+        try {
+          selected.forEach(async (value, index) => {
+            const hasRole = member.roles.cache.has(value);
+
+            if (hasRole) {
+              await member.roles.remove(value);
+              completedRoles.push({ roleId: value, action: "removed" });
+              console.log(
+                `[${interaction.client.function.timeUtils.timeToJSTstamp(
+                  Date.now(),
+                  true
+                )} info] -> Role Removed: for ${member.displayName}`
+              );
+            } else {
+              await member.roles.add(value);
+              completedRoles.push({ roleId: value, action: "added" });
+              console.log(
+                `[${interaction.client.function.timeUtils.timeToJSTstamp(
+                  Date.now(),
+                  true
+                )} info] -> Role Added: for ${member.displayName}`
+              );
+            }
+            if (index === selected.length - 1) {
+              completed = true;
+            }
+          });
+        } catch (error) {
+          console.error(
+            `[${interaction.client.function.timeUtils.timeToJSTstamp(
+              Date.now(),
+              true
+            )} error]An Error Occured in ${interaction.customId}\nDetails:\n${error}`
+          );
+          const messageEmbed = new EmbedBuilder()
+            .setTitle("すみません。エラーが発生しました。")
+            .setDescription("```\n" + error + "\n```")
+            .setColor(config.color.e)
+            .setTimestamp();
+          await interaction.followUp({
+            embeds: [messageEmbed],
+            flags: MessageFlags.Ephemeral,
+          });
+          if (completedRoles.length > 0) {
+            const completedRolesString = completedRoles
+              .map((role) => {
+                return `- <@&${role.roleId}> を ${role.action == "added" ? "追加" : "削除"}`;
+              })
+              .join("\n");
+            await interaction.followUp({
+              content: `## 次のとおり変更が完了しました。
+<@${interaction.user.id}>さんのロールから
+${completedRolesString}
+と変更しました。`,
+              flags: MessageFlags.Ephemeral,
+            });
+          }
+          return;
+        }
+        while (!completed) {
+          await new Promise((resolve) => setTimeout(resolve, 100));
+        }
+        if (completedRoles.length > 0) {
+          const completedRolesString = completedRoles
+            .map((role) => {
+              return `- <@&${role.roleId}> を ${role.action == "added" ? "追加" : "削除"}`;
+            })
+            .join("\n");
+          await interaction.followUp({
+            content: `## 次のとおり変更が完了しました。
+<@${interaction.user.id}>さんのロールから
+${completedRolesString}
+と変更しました。`,
+            flags: MessageFlags.Ephemeral,
+          });
+        }
+      } else {
+        console.log(
+          `[${interaction.client.function.timeUtils.timeToJSTstamp(
+            Date.now(),
+            true
+          )} info] Not Found: ${interaction.customId}`
+        );
+      }
+    } catch (error) {
+      console.error(
         `[${interaction.client.function.timeUtils.timeToJSTstamp(
           Date.now(),
           true
-        )} info] Not Found: ${interaction.customId}`
+        )} error]An Error Occured in ${interaction.customId}\nDetails:\n${error}`
       );
+      const logEmbed = new EmbedBuilder()
+        .setTitle("ERROR - cmd")
+        .setDescription("```\n" + (error as any).toString() + "\n```")
+        .setColor(config.color.e)
+        .setTimestamp();
+
+      const channel = await GetErrorChannel(interaction.client);
+      if (channel) {
+        channel.send({ embeds: [logEmbed] });
+      }
+      const messageEmbed = new EmbedBuilder()
+        .setTitle("すみません。エラーが発生しました。")
+        .setDescription("```\n" + error + "\n```")
+        .setColor(config.color.e)
+        .setTimestamp();
+      if (interaction.channel && interaction.channel.isSendable()) {
+        await interaction.channel.send({ embeds: [messageEmbed] });
+      }
+      const logChannel = await GetLogChannel(interaction.client);
+      if (logChannel) {
+        logChannel.send({ embeds: [messageEmbed] });
+      }
     }
   }
 };
