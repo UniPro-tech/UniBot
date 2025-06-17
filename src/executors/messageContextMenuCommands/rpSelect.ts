444916696f3909c5e14be71b74bcf1da61369aa3
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-06-18 01:12:34 +0900

    ✨ selectedDataモデルを追加し、データの書き込み機能を実装。RP選択コマンドに応じた応答を強化。
    Co-authored-by: ayane0857 <tailangrongyi@gmail.com>

--- src/executors/messageContextMenuCommands/rpSelect.ts
+++ src/executors/messageContextMenuCommands/rpSelect.ts
@@ -2,7 +2,11 @@ import {
   ContextMenuCommandBuilder,
   ApplicationCommandType,
   MessageContextMenuCommandInteraction,
+  MessageFlags,
+  EmbedBuilder,
 } from "discord.js";
+import { SelectedData, writeSelected } from "@/lib/dataUtils.js";
+import config from "@/config.js";
 
 export const name = "RPを選択";
 
@@ -12,6 +16,37 @@ export const data = new ContextMenuCommandBuilder()
   .setType(ApplicationCommandType.Message);
 
 export const execute = async (interaction: MessageContextMenuCommandInteraction) => {
-  interaction.reply("OK");
+  if (
+    interaction.targetMessage.author.id != interaction.client.user.id ||
+    !(
+      interaction.targetMessage.components[0] &&
+      interaction.targetMessage.components[0].type == 1 &&
+      interaction.targetMessage.components[0].components[0].customId?.startsWith("rp_")
+    )
+  ) {
+    const messageEmbed = new EmbedBuilder()
+      .setTitle("ロールパネルではありません")
+      .setColor(config.color.e)
+      .setTimestamp();
+    await interaction.reply({
+      embeds: [messageEmbed],
+      flags: [MessageFlags.Ephemeral],
+    });
+    return;
+  }
+  const messageId = interaction.targetMessage.id;
+  writeSelected({
+    user: interaction.user.id,
+    type: "Message",
+    data: interaction.targetMessage.id,
+  } as SelectedData);
+  const messageEmbed = new EmbedBuilder()
+      .setTitle("ロールパネルを選択しました")
+      .setColor(config.color.s)
+      .setTimestamp();
+  await interaction.reply({
+    embeds: [messageEmbed],
+    flags: [MessageFlags.Ephemeral],
+  });
   return;
 };
--- src/lib/dataUtils.ts
+++ src/lib/dataUtils.ts
@@ -1,6 +1,6 @@
 import timeUtils from "@/lib/timeUtils";
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, selectedData } from "@prisma/client";
 
 export const prismaClient = new PrismaClient();
 
@@ -39,6 +39,60 @@ export const readConfig = async (key: string) => {
         true
       )} error]An Error Occured.\nDatails:\n${error}\u001b[0m`
     );
+  }
+};
+
+export enum SelectedDataType {
+  Message = "Message",
+  User = "User",
+}
+
+export type SelectedData = {
+  id?: string;
+  user: string;
+  type: SelectedDataType;
+  data: string;
+};
+
+/**
+ * Writs a log file for a specific directory.
+ * @param {string} api_name - The name of the path.
+ * @returns {Promise<Object>} - The parsed log data.
+ */
+export const writeSelected = async (data: SelectedData): Promise<void> => {
+  try {
+    await prismaClient.selectedData.create({
+      data: {
+        user: data.user,
+        type: data.type,
+        data: JSON.stringify(data.data),
+      },
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+/**
+ * Reads a log file for a specific directory.
+ * @param {string} api_name - The name of the path.
+ * @returns {Promise<Object>} - The parsed log data.
+ */
+export const readSelected = async (id: string): Promise<SelectedData | null> => {
+  try {
+    const selectedData = await prismaClient.selectedData.findUnique({
+      where: { id },
+    });
+    return selectedData ? JSON.parse(selectedData.data) : null;
+  } catch (error) {
+    // TODO: ここでThrow Errorを投げて、呼び出し元でcatchするようにする
+    console.error(
+      `\u001b[31m[${timeUtils.timeToJSTstamp(
+        Date.now(),
+        true
+      )} error]An Error Occured.\nDatails:\n${error}\u001b[0m`
+    );
+    return null;
   }
 };
 
