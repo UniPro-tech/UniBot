789f048e65054cda741e9ff3ef3a3e78aa47296a
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-01-20 00:37:37 +0900

    リレーションの定義

--- src/internal/model/member.go
+++ src/internal/model/member.go
@@ -4,4 +4,5 @@ type Member struct {
 	DiscordID           string              `gorm:"primaryKey;size:255"`
 	Guilds              []*Guild            `gorm:"many2many:members_guilds;"`
 	TTSPersonalSettings *TTSPersonalSetting `gorm:"foreignKey:AuthorID;references:DiscordID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ScheduleSetting     []*ScheduleSetting  `gorm:"foreignKey:ID;references:DiscordID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
