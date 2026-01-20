5a39ccd32b222f064b88d65aa836d62a22de76d4
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-01-20 23:48:52 +0900

    ディレクトリ構成の変更

--- src/internal/bot/command/registry.go
+++ src/internal/bot/command/registry.go
@@ -1,8 +1,12 @@
 package command
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"unibot/internal/bot/command/general"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 var Handlers = map[string]func(*discordgo.Session, *discordgo.InteractionCreate){
-	"ping":  Ping,
-	"about": About,
+	"ping":  general.Ping,
+	"about": general.About,
 }
