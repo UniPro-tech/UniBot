a3609c243327b2a8274e9c1d343e239ec5dcd229
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-01-22 20:29:12 +0900

    add build script template

---
+++ scripts/_build.sh
@@ -0,0 +1,20 @@
+COMMIT=$(git rev-parse --short HEAD)
+BRANCH=$(git branch --show-current)
+
+if [[ "$*" == *"--dev"* ]]; then
+  DISCORD_TOKEN="your_token_here"
+  CONFIG_ADMIN_GUILD_ID="your_guild_id_here"
+  CONFIG_ADMIN_ROLE_ID="your_role_id_here"
+
+  go run -ldflags "\
+-X unibot/internal.GitCommit=$COMMIT \
+-X unibot/internal.GitBranch=$BRANCH" \
+cmd/bot/main.go
+else
+  VERSION=$(git describe --tags --abbrev=0)
+
+  go build -ldflags "\
+-X unibot/internal.Version=$VERSION \
+-X unibot/internal.GitCommit=$COMMIT \
+-X unibot/internal.GitBranch=$BRANCH"
+fi
