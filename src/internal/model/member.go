11351b0f25e2daab359aa90b36b3fea251f7834b
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-01-22 23:03:08 +0900

    不要なリレーションの削除

--- src/internal/model/member.go
+++ src/internal/model/member.go
@@ -1,8 +1,7 @@
 package model
 
 type Member struct {
-	DiscordID          string              `gorm:"primaryKey;size:255"`
-	Guilds             []*Guild            `gorm:"many2many:members_guilds;"`
-	TTSPersonalSetting *TTSPersonalSetting `gorm:"foreignKey:MemberID;references:DiscordID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	ScheduleSetting    []*ScheduleSetting  `gorm:"foreignKey:AuthorID;references:DiscordID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	DiscordID       string             `gorm:"primaryKey;size:255"`
+	Guilds          []*Guild           `gorm:"many2many:members_guilds;"`
+	ScheduleSetting []*ScheduleSetting `gorm:"foreignKey:AuthorID;references:DiscordID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
--- src/internal/model/tts_personal_setting.go
+++ src/internal/model/tts_personal_setting.go
@@ -1,11 +1,10 @@
 package model
 
 type TTSPersonalSetting struct {
-	ID          uint   `gorm:"primaryKey;autoIncrement"`
-	SpeakerID   string `gorm:"primaryKey;size:64"`
-	SpeakerSeed int64  `gorm:"primaryKey;"`
-	CreatedAt   int64  `gorm:"autoCreateTime:nano"`
-	UpdatedAt   int64  `gorm:"autoUpdateTime:nano"`
-	MemberID    string `gorm:"size:64;index;not null;"`
-	Member      Member `gorm:"foreignKey:MemberID;references:DiscordID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ID           uint   `gorm:"primaryKey;autoIncrement"`
+	SpeakerID    string `gorm:"size:16;not null;"`
+	SpeakerPitch int64
+	CreatedAt    int64  `gorm:"autoCreateTime:nano"`
+	UpdatedAt    int64  `gorm:"autoUpdateTime:nano"`
+	MemberID     string `gorm:"size:64;index;not null;"`
 }
