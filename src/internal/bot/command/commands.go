b57ec0306f791dfb88d1d095695a18510022980e
Aqua-218 <aqua@arivell.com>
2026-01-28 21:21:57 +0900

    pinコマンド登録

--- src/internal/bot/command/commands.go
+++ src/internal/bot/command/commands.go
@@ -10,6 +10,9 @@ import (
 var Commands = []*discordgo.ApplicationCommand{
 	general.LoadPingCommandContext(),
 	general.LoadAboutCommandContext(),
+	general.LoadPinCommandContext(),
+	general.LoadUnpinCommandContext(),
+	general.LoadPinSelectCommandContext(),
 	general.LoadTtsCommandContext(),
 	general.LoadScheduleCommandContext(),
 	general.LoadHelpCommandContext(),
