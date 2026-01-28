5def9fbcb58b41c4e27c847bdfd3c8da85d1b6a0
Aqua-218 <aqua@arivell.com>
2026-01-28 21:22:02 +0900

    pinハンドラ登録

--- src/internal/bot/command/registry.go
+++ src/internal/bot/command/registry.go
@@ -11,6 +11,10 @@ import (
 var Handlers = map[string]func(*internal.BotContext, *discordgo.Session, *discordgo.InteractionCreate){
 	"ping":        general.Ping,
 	"about":       general.About,
+	"pin":         general.Pin,
+	"unpin":       general.Unpin,
+	"Pinするメッセージを選択": general.PinSelect,
+	"schedule":    general.Schedule,
 	"maintenance": admin.Maintenance,
 	"tts":         general.Tts,
 	"help":        general.Help,
