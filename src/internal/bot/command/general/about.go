ff2bf16166eed3602c458430e5a3b883a01ebe28
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-05-06 12:19:07 +0900

    aboutコマンドを追加

--- src/internal/bot/command/general/about.go
+++ src/internal/bot/command/general/about.go
@@ -1,113 +1,73 @@
 package general
 
 import (
-	"log"
 	"time"
 
-	"github.com/bwmarrin/discordgo"
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/handler"
 
 	"unibot/internal"
 )
 
-func LoadAboutCommandContext() *discordgo.ApplicationCommand {
-	return &discordgo.ApplicationCommand{
+func LoadAboutCommandContext() discord.SlashCommandCreate {
+	return discord.SlashCommandCreate{
 		Name:        "about",
 		Description: "ボットの情報を表示します",
 	}
 }
 
-func About(ctx *internal.BotContext, s *discordgo.Session, i *discordgo.InteractionCreate) {
-	config := ctx.Config
-	done := make(chan struct{})
-	go func() {
-		select {
-		case <-done:
-			return
-		case <-time.After(3 * time.Minute):
-			_, err := s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
-				Embeds: &[]*discordgo.MessageEmbed{
-					{
-						Title:       "エラー",
-						Description: "情報の表示に失敗しました。",
-						Color:       config.Colors.Error,
-						Footer: &discordgo.MessageEmbedFooter{
-							Text:    "Requested by " + i.Member.DisplayName(),
-							IconURL: i.Member.AvatarURL(""),
-						},
-						Timestamp: time.Now().Format(time.RFC3339),
-					},
-				},
-			})
-			if err != nil {
-				log.Println("Failed to edit deferred interaction on timeout:", err)
+func About(ctx *internal.BotContext) func(data discord.SlashCommandInteractionData, e *handler.CommandEvent) error {
+	return func(data discord.SlashCommandInteractionData, e *handler.CommandEvent) error {
+		config := ctx.Config
+		// コントリビューター一覧をMarkdown形式で作成
+		contributorsText := ""
+		// Botは最後にする
+		for _, contributor := range config.Contributors {
+			if contributor.IsBot {
+				continue
 			}
+			contributorsText += "- [" + contributor.Username + "](" + contributor.Profile + ")\n"
 		}
-	}()
-	defer close(done)
-	// コントリビューター一覧をMarkdown形式で作成
-	contributorsText := ""
-	// Botは最後にする
-	for _, contributor := range config.Contributors {
-		if contributor.IsBot {
-			continue
+		for _, contributor := range config.Contributors {
+			if !contributor.IsBot {
+				continue
+			}
+			contributorsText += "- [" + contributor.Username + "](" + contributor.Profile + ")\n"
 		}
-		contributorsText += "- [" + contributor.Username + "](" + contributor.Profile + ")\n"
-	}
-	for _, contributor := range config.Contributors {
-		if !contributor.IsBot {
-			continue
-		}
-		contributorsText += "- [" + contributor.Username + "](" + contributor.Profile + ")\n"
-	}
 
-	responseEmbed := &discordgo.MessageEmbed{
-		Title:       "About " + config.BotName + " 🤖",
-		Description: config.Description,
-		Color:       config.Colors.Primary,
-		Fields: []*discordgo.MessageEmbedField{
-			{
-				Name:  "Version",
-				Value: config.BotVersion,
-			},
-			{
-				Name:  "Contributors",
-				Value: contributorsText,
-			},
-			{
-				Name:  "GitHub",
-				Value: config.GitHub,
-			},
-			{
-				Name:  "Support Server",
-				Value: config.SupportServer,
-			},
-		},
-		Footer: &discordgo.MessageEmbedFooter{
-			Text:    "Requested by " + i.Member.DisplayName(),
-			IconURL: i.Member.AvatarURL(""),
-		},
-		Timestamp: time.Now().Format(time.RFC3339),
-	}
-
-	_, err := s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
-		Embeds: &[]*discordgo.MessageEmbed{responseEmbed},
-	})
-	if err != nil {
-		s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
-			Embeds: &[]*discordgo.MessageEmbed{
+		responseEmbed := discord.Embed{
+			Title:       "About " + config.BotName + " 🤖",
+			Description: config.Description,
+			Color:       config.Colors.Primary,
+			Fields: []discord.EmbedField{
 				{
-					Title:       "エラー",
-					Description: "情報を表示できませんでした。",
-					Color:       config.Colors.Error,
-					Footer: &discordgo.MessageEmbedFooter{
-						Text:    "Requested by " + i.Member.DisplayName(),
-						IconURL: i.Member.AvatarURL(""),
-					},
-					Timestamp: time.Now().Format(time.RFC3339),
+					Name:  "Version",
+					Value: config.BotVersion,
+				},
+				{
+					Name:  "Contributors",
+					Value: contributorsText,
+				},
+				{
+					Name:  "GitHub",
+					Value: config.GitHub,
+				},
+				{
+					Name:  "Support Server",
+					Value: config.SupportServer,
 				},
 			},
-			Flags: discordgo.MessageFlagsEphemeral,
-		})
-		return
+			Footer: &discord.EmbedFooter{
+				Text:    "Requested by " + e.User().Username,
+				IconURL: e.User().EffectiveAvatarURL(),
+			},
+			Timestamp: func() *time.Time {
+				t := time.Now()
+				return &t
+			}(),
+		}
+
+		_, err := e.Client().Rest.CreateFollowupMessage(e.ApplicationID(), e.Token(), discord.NewMessageCreate().WithEmbeds(responseEmbed))
+		return err
 	}
 }
