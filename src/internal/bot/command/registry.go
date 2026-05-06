d00e43abcb647e3e098d80549d2a40aff8f31c01
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-05-06 12:18:54 +0900

    defer replyする

--- src/internal/bot/command/registry.go
+++ src/internal/bot/command/registry.go
@@ -6,8 +6,6 @@ import (
 	"unibot/internal"
 	"unibot/internal/bot/command/admin/maintenance"
 	"unibot/internal/bot/command/general"
-	"unibot/internal/bot/command/general/tts"
-	"unibot/internal/bot/command/general/tts/ttsSet"
 
 	"github.com/disgoorg/disgo/discord"
 	"github.com/disgoorg/disgo/handler"
@@ -15,17 +13,18 @@ import (
 )
 
 func RegistHandler(r *handler.Mux, ctxData *internal.BotContext) {
+	r.Use(DeferReplyMiddleware(ctxData, false))
 	r.SlashCommand("/ping", general.Ping(ctxData))
 	r.SlashCommand("/about", general.About(ctxData))
 	r.SlashCommand("/help", general.Help(ctxData))
 	r.SlashCommand("/colorcode", general.ColorCode(ctxData))
-	r.Route("/tts", func(r handler.Router) {
+	/*r.Route("/tts", func(r handler.Router) {
 		r.SlashCommand("/join", tts.Join(ctxData))
 		r.Route("/set", func(r handler.Router) {
 			r.SlashCommand("/speed", ttsSet.Speed(ctxData))
 			r.SlashCommand("/voice", ttsSet.Speed(ctxData))
 		})
-	})
+	})*/
 	r.Route("/maintenance", func(r handler.Router) {
 		r.Use(AdminOnlyMiddleware(ctxData))
 		r.SlashCommand("/status/set", maintenance.StatusSetHandler(ctxData))
@@ -44,14 +43,14 @@ func AdminOnlyMiddleware(ctx *internal.BotContext) func(next handler.Handler) ha
 	return func(next handler.Handler) handler.Handler {
 		return func(e *handler.InteractionEvent) error {
 			config := ctx.Config
-			if !IsOwner(*&e.Member().Member) {
+			if !IsOwner(e.Member().Member) {
 				errorEmbed := discord.Embed{
 					Title:       "権限エラー",
 					Description: "権限がありません。",
 					Color:       config.Colors.Error,
 					Footer: &discord.EmbedFooter{
 						Text:    "Requested by " + *e.Member().Nick,
-						IconURL: *e.Member().Avatar,
+						IconURL: e.User().EffectiveAvatarURL(),
 					},
 					Timestamp: func() *time.Time {
 						t := time.Now()
@@ -62,6 +61,15 @@ func AdminOnlyMiddleware(ctx *internal.BotContext) func(next handler.Handler) ha
 				return err
 			}
 
+			return next(e)
+		}
+	}
+}
+
+func DeferReplyMiddleware(ctx *internal.BotContext, ephemeral bool) func(next handler.Handler) handler.Handler {
+	return func(next handler.Handler) handler.Handler {
+		return func(e *handler.InteractionEvent) error {
+			e.DeferCreateMessage(ephemeral)
 			return next(e)
 		}
 	}
