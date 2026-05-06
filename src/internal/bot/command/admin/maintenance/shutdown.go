7e00084fd6f3d5efa64c37b8a26c2023de83c9e1
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-05-06 10:28:02 +0900

    シャットダウンコマンドをdisgoに換装

---
+++ src/internal/bot/command/admin/maintenance/shutdown.go
@@ -0,0 +1,54 @@
+package maintenance
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"time"
+	"unibot/internal"
+
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/handler"
+)
+
+func LoadShutdownCommandContext() discord.ApplicationCommandOptionSubCommand {
+	return discord.ApplicationCommandOptionSubCommand{
+		Name:        "shutdown",
+		Description: "Botプロセスをシャットダウンします。(コンテナの場合は再起動を兼ねます)",
+	}
+}
+
+func Shutdown(ctx *internal.BotContext) func(data discord.SlashCommandInteractionData, e *handler.CommandEvent) error {
+	return func(data discord.SlashCommandInteractionData, e *handler.CommandEvent) error {
+		config := ctx.Config
+
+		responseEmbed := discord.Embed{
+			Title:       "Now Shutting down...",
+			Description: "The bot is Shutting down...",
+			Color:       config.Colors.Success,
+			Footer: &discord.EmbedFooter{
+				Text:    fmt.Sprintf("Requested by %s", e.User().Username),
+				IconURL: *e.Member().Avatar,
+			},
+			Timestamp: func() *time.Time {
+				t := time.Now()
+				return &t
+			}(),
+		}
+
+		_, err := e.Client().Rest.CreateFollowupMessage(e.ApplicationID(), e.Token(), discord.NewMessageCreate().WithEmbeds(responseEmbed))
+
+		// 再起動と称してプロセスを終了する
+		// コンテナで起動することを想定しているため、再起動となる
+		go func() {
+			time.Sleep(2 * time.Second)
+			p, err := os.FindProcess(os.Getpid())
+			if err != nil {
+				return
+			}
+			p.Signal(syscall.SIGTERM)
+		}()
+
+		return err
+	}
+}
