ac685c2daa53a6e6c59a47ce924aeef4492132cf
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-07-25 22:49:43 +0900

    fix: Gen Queryに移行
    Signed-off-by: Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>

--- src/internal/bot/handlers/event/ready.go
+++ src/internal/bot/handlers/event/ready.go
@@ -6,6 +6,7 @@ import (
 
 	"unibot/internal"
 	"unibot/internal/bot/handlers/interaction/command/admin/maintenance"
+	"unibot/internal/query"
 
 	"github.com/disgoorg/disgo/discord"
 	"github.com/disgoorg/disgo/events"
@@ -15,53 +16,64 @@ func Ready(ctx *internal.BotContext, e *events.Ready) {
 	log.Println("Bot is ready 🚀")
 	log.Printf("Logged in as: %v#%v", e.User.Username, e.User.Discriminator)
 
-	// Get DB Connection
-	//database := ctx.DB
-
 	// Get status
-	/*
-		settingsRepo := repository.NewBotSystemSettingRepository(database)
-		botStatusRecord, err := settingsRepo.GetByID("status")
-		if err != nil {
-			return
-		} else {
-			if botStatusRecord == nil {
-	*/
-	serverCounts := len(e.Guilds)
-	statusData := maintenance.StatusData{
-		Text:         fmt.Sprintf("Serving %d servers | /help", serverCounts),
-		Type:         discord.ActivityTypeGame,
-		OnlineStatus: discord.OnlineStatusOnline,
-	}
-	if err := maintenance.SetBotStatus(e.Client(), statusData); err != nil {
+	systemPreference, err := query.SystemPreference.FirstOrInit()
+	if err != nil {
 		return
-	}
-	/*
-				newSetting := &model.BotSystemSetting{ID: "status"}
-				if err := newSetting.Value.Set(statusData); err != nil {
-					return
-				}
-				if err := settingsRepo.Create(newSetting); err != nil {
-					log.Printf("Error creating default bot status: %v", err)
-				}
-			} else {
-				botStatus := botStatusRecord.Value
-
-				// jsonからデータを引き出す処理を追加
-				var statusData maintenance.StatusData
-				err = json.Unmarshal(botStatus.Bytes, &statusData)
-
-				if err != nil {
-					return
-				} else {
-					// Set Bot Status
-					err = maintenance.SetBotStatus(e.Client(), statusData)
-					if err != nil {
-						log.Printf("Error setting bot status: %v", err)
-						return
-					}
-				}
-			}
+	} else {
+		if systemPreference.ActivityType == nil {
+			t := "game"
+			systemPreference.ActivityType = &t
 		}
-	*/
+		if systemPreference.ActivitySummary == nil {
+			serverCounts := len(e.Guilds)
+			s := fmt.Sprintf("Serving %d servers | /help", serverCounts)
+			systemPreference.ActivitySummary = &s
+		}
+		statusData := maintenance.StatusData{
+			Text: *systemPreference.ActivitySummary,
+		}
+
+		// activity type switch
+		activityType := "game"
+		if systemPreference.ActivityType != nil {
+			activityType = *systemPreference.ActivityType
+		}
+		switch activityType {
+		case "custom":
+			statusData.Type = discord.ActivityTypeCustom
+		case "game":
+			statusData.Type = discord.ActivityTypeGame
+		case "competing":
+			statusData.Type = discord.ActivityTypeCompeting
+		case "watching":
+			statusData.Type = discord.ActivityTypeWatching
+		case "listening":
+			statusData.Type = discord.ActivityTypeListening
+		case "streaming":
+			statusData.Type = discord.ActivityTypeStreaming
+		default:
+			statusData.Type = discord.ActivityTypeGame
+		}
+
+		// status type switch
+		switch systemPreference.StatusType {
+		case "online":
+			statusData.OnlineStatus = discord.OnlineStatusOnline
+		case "dnd":
+			statusData.OnlineStatus = discord.OnlineStatusDND
+		case "idle":
+			statusData.OnlineStatus = discord.OnlineStatusIdle
+		case "invisible":
+			statusData.OnlineStatus = discord.OnlineStatusInvisible
+		case "offline":
+			statusData.OnlineStatus = discord.OnlineStatusOffline
+		default:
+			statusData.OnlineStatus = discord.OnlineStatusOnline
+		}
+		if err := maintenance.SetBotStatus(e.Client(), statusData); err != nil {
+			log.Printf("Error setting bot status: %v", err)
+			return
+		}
+	}
 }
